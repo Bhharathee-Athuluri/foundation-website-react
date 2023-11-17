fdd9e2ecc7cc5b7cc44a62e2998b6911818b948a
Bhharathee-Athuluri <abhanu92002@gmail.com>
2023-11-18 00:08:50 +0530

    update: Footer Styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,20 @@ import { photo3 } from '../assets'
 const Footer = () => {
   return (
     <section id="footer" className={`flex md:flex-row flex-col p-3`}>
-        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-          <div className="flex flex-row items-center py-[6px] px-4 bg-white rounded-[10px] mb-2">
-            <h1 className='flex-1 font-poppins font-semibold ss:text-[44px] text-[30px] ss:leading-[40px] leading-[25px]'>Get in Touch</h1>
+        <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 items-center sm:px-16 px-6`}>
+          <div className="flex flex-row py-[6px] px-4 rounded mb-2">
+            <h1 className='flex-1 font-poppins font-semibold ss:text-[36px] text-[40px] ss:leading-[40px] leading-[25px]'>Get in Touch</h1>
           </div>
-          <ul className='list-none'>
-            <li>career@nayepankh.com</li>
-            <li>Calling: +91- 8318500748</li>
-            <li>WhatsApp: +91-6306266557</li>
-            <li>Office Address: 104A/279 P.Road Kanpur</li>
-            <li>Pin Code: 208012</li>                                    
+          <ul className='list-none font-poppins whitespace-nowrap p-2'>
+            <li><b>Mail us at:</b> <a href = "mailto: career@nayepankh.com">career@nayepankh.com</a></li>
+            <li><b>Calling:</b> +91- 8318500748</li>
+            <li><b>WhatsApp:</b> +91-6306266557</li>
+            <li><b>Our Office Address:</b> 104A/279, P.Road, Kanpur</li>
+            <li><b>Pin Code:</b> 208012</li>                                    
           </ul>
           </div>
-          <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative p-3`}>
-            <img src={photo3} alt="Photo-3" className='w-[70%] h-[70%] relative z-[5]' />
+          <div className={`flex-1 flex ${styles.flexCenter} relative p-2`}>
+            <img src={photo3} alt="Photo-3" className='w-[75%] h-[75%] relative z-[6]' />
         </div>
       
     </section>
